a7965babafbf9a3cb23aeee9be003d3124d7d20e
Emran Hossain <emranhossain0335@gmail.com>
2024-10-04 12:14:52 +0600

    navbar is set

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,8 +6,8 @@ function Search() {
     <div className="mr-32 w-[60%] text-black">
       <label htmlFor="search-input" className="flex items-center relative">
         <FaSearch className="absolute left-3 text-black h-4 w-4" />
-        <input type="text" id="search-input" className="w-full rounded-md py-1 px-8 outline-none" placeholder="Search the content"/>
-        <button className="absolute text-black right-0 border h-full rounded-l-0 rounded-tr-md rounded-br-md px-2 border-l-1">
+        <input type="text" id="search-input" className="w-full rounded-full py-1.5 px-9 outline-none" placeholder="Please Search"/>
+        <button className="absolute text-black right-0 border h-full rounded-l-0 rounded-tr-full rounded-br-full px-4 border-l-1">
           Search
         </button>
       </label>
